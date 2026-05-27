ad94b83498700671b47381fba27702eb5d9d7f81
Samantha Hellen <samanthahellencl@gmail.com>
2026-05-27 20:28:17 -0300

    security corrigido

--- Projeto Flix/src/main/java/br/uniesp/si/techback/config/SecurityConfig.java
+++ Projeto Flix/src/main/java/br/uniesp/si/techback/config/SecurityConfig.java
@@ -2,20 +2,25 @@ package br.uniesp.si.techback.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
-    // bean necessário para fazer o hash da senha no UsuarioService
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-}
-@Bean
-public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-    return http.build();
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+
 }
--- Projeto Flix/src/main/java/br/uniesp/si/techback/mapper/UsuarioMapper.java
+++ Projeto Flix/src/main/java/br/uniesp/si/techback/mapper/UsuarioMapper.java
@@ -6,12 +6,28 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UsuarioMapper {
+
     public Usuario toEntity(UsuarioDTO dto) {
-        return Usuario.builder().id(dto.getId()).nome(dto.getNome())
-                .email(dto.getEmail()).senha(dto.getSenha()).build();
+        return Usuario.builder()
+                .id(dto.getId())
+                .nomeCompleto(dto.getNomeCompleto())
+                .dataNascimento(dto.getDataNascimento())
+                .email(dto.getEmail())
+                .senhaHash(dto.getSenha())
+                .cpfCnpj(dto.getCpfCnpj())
+                .perfil(dto.getPerfil())
+                .build();
     }
+
     public UsuarioDTO toDTO(Usuario u) {
-        return UsuarioDTO.builder().id(u.getId()).nome(u.getNome())
-                .email(u.getEmail()).senha(u.getSenha()).build();
+        return UsuarioDTO.builder()
+                .id(u.getId())
+                .nomeCompleto(u.getNomeCompleto())
+                .dataNascimento(u.getDataNascimento())
+                .email(u.getEmail())
+                .senha(u.getSenhaHash())
+                .cpfCnpj(u.getCpfCnpj())
+                .perfil(u.getPerfil())
+                .build();
     }
 }
